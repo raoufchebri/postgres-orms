93d3c6e961ec108be7310b97a64017cfd20e6330
raoufchebri <raouf@chebri.com>
2023-05-17 13:58:47 +0200

    drizzle edge

--- pages/api/drizzle-edge.ts
+++ pages/api/drizzle-edge.ts
@@ -1,6 +1,6 @@
 import { Pool } from '@neondatabase/serverless';
-import { Kysely, PostgresDialect, sql } from 'kysely';
-import type { DB } from '../../kysely-types';
+import { periodicTable } from '../../drizzle-types';
+import { drizzle } from 'drizzle-orm/neon-serverless';
 
 export const config = {
   runtime: 'edge',
@@ -9,13 +9,8 @@ export const config = {
 
 export default async (req: Request, ctx: any) => {
   const pool = new Pool({ connectionString: process.env.KYSELY_DATABASE_URL });
-  const db = new Kysely<DB>({ dialect: new PostgresDialect({ pool }) });
-  const query = db
-    .selectFrom('atoms')
-    .selectAll()
-    .limit(10);
-
-  const atoms = await query.execute();
+  const db = drizzle(pool);
+  const atoms = await db.select().from(periodicTable);
   ctx.waitUntil(pool.end());
 
   return new Response(JSON.stringify({ atoms }));
